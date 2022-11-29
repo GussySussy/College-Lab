3ee8f204795ff76ec6527f904a9fde35d986f838
GussySussy <gourisankar.shaji@gmail.com>
2022-11-30 00:19:29 +0530

    make edits to linkedlist file

--- Sem 3/Cycle 2/LinkedList.c
+++ Sem 3/Cycle 2/LinkedList.c
@@ -7,7 +7,7 @@ typedef struct
     struct node *link;
 } node;
 
-*head = NULL;
+node *head = NULL;
 
 void insertAtBeginning(int data)
 {
@@ -16,7 +16,7 @@ void insertAtBeginning(int data)
     newNode->link = head;
     head = newNode;
 
-    printf("--- Insertion is successfull ---");
+    printf("\n--- Insertion is successfull ---");
 }
 
 void insertAtEnd(int data)
@@ -37,10 +37,109 @@ void insertAtEnd(int data)
         }
         end->link = newNode;
     }
-    printf("--- Insertion is successful ---");
+    printf("\n--- Insertion is successful ---");
 }
 
 void insertAfterValue(int pos, int data)
 {
     node *newNode = (node *)malloc(sizeof(node));
+    newNode->data = data;
+
+    node *curr = head;
+    while (curr != NULL && --pos)
+    {
+        curr = curr->link;
+    }
+    if (curr == NULL)
+    {
+        printf("\nposition exceeds the size of the list");
+    }
+    else
+    {
+        newNode->link = curr->link;
+        curr->link = newNode;
+    }
+}
+
+void deleteByValue(int value)
+{
+    node *curr = head, *prev = NULL;
+    while (curr != NULL && curr->data != value)
+    {
+        prev = curr;
+        curr = curr->link;
+    }
+    if (head->data == value)
+    {
+        head = head->link;
+    }
+    else if (curr == NULL)
+    {
+        printf("\nElement not found in the List");
+    }
+    prev->link = curr->link;
+    free(curr);
+    printf("\nDeletion Successfull");
+}
+
+void display()
+{
+    node *curr = head;
+    if (curr == NULL)
+    {
+        printf("\nList empty");
+    }
+    else
+    {
+        while (curr != NULL)
+        {
+            printf("%d\t", curr->data);
+            curr = curr->link;
+        }
+    }
+}
+
+void main()
+{
+    int ch, element, pos;
+    do
+    {
+        printf("\n\nChoose operation");
+        printf("\n1.Insert at beginning");
+        printf("\n2.Insert at end");
+        printf("\n3.Insert at position");
+        printf("\n4.Delete node containing a particular element ");
+        printf("\n5.Display linked list");
+
+        printf("\n\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\nEnter element to insert ");
+            scanf("%d", &element);
+            insertAtBeginning(element);
+            break;
+        case 2:
+            printf("\nEnter element to insert  ");
+            scanf("%d", &element);
+            insertAtEnd(element);
+            break;
+        case 3:
+            printf("\nEnter the position after which node will be inserted ");
+            scanf("%d", &pos);
+            printf("\nEnter element to insert ");
+            scanf("%d", &element);
+            insertAfterPosition(pos, element);
+            break;
+        case 4:
+            printf("\nEnter element to delete ");
+            scanf("%d", &element);
+            deleteElement(element);
+            break;
+        case 5:
+            display();
+            break;
+        }
+    } while (ch < 6);
 }
