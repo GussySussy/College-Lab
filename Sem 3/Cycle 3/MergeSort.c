4bcce353b121a3a1f07b448a1529f48a28b0fc81
GussySussy <gourisankar.shaji@gmail.com>
2022-12-01 23:10:01 +0530

    fixed recursion and segmentation error merge sort complete

--- Sem 3/Cycle 3/MergeSort.c
+++ Sem 3/Cycle 3/MergeSort.c
@@ -2,11 +2,11 @@
 
 int a[100], b[100], l, r;
 
-void merge(int a[], int low, int mid, int high)
+void merge(int low, int mid, int high)
 {
     int l, r, k;
 
-    for (l = low, r = mid + 1; l <= mid && r <= high; k++)
+    for (l = low, r = mid + 1, k = low; l <= mid && r <= high; k++)
     {
         if (a[l] <= a[r])
         {
@@ -23,7 +23,7 @@ void merge(int a[], int low, int mid, int high)
 
     while (r <= high)
     {
-        b[k++] = b[r++];
+        b[k++] = a[r++];
     }
 
     for (k = low; k <= high; k++)
@@ -34,19 +34,24 @@ void merge(int a[], int low, int mid, int high)
 
 void sort(int low, int high)
 {
-    int mid = (low + high) / 2;
-    sort(low, mid);
-    sort(mid + 1, high);
-    merge(a, low, mid, high);
+    int mid;
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        sort(low, mid);
+        sort(mid + 1, high);
+        merge(low, mid, high);
+    }
+    else
+    {
+        return;
+    }
 }
 
 void display(int a[], int length)
 {
-    int i = 0;
-    while (i < length)
-    {
-        printf("%d\t", a[i++]);
-    }
+    for (int i = 0; i < length; i++)
+        printf("%d\t", a[i]);
 }
 
 void main()
@@ -62,10 +67,10 @@ void main()
         printf("Do you wish to add another element to the list?(y/n) : \n");
         scanf(" %c", &ch);
     } while (ch == 'y');
-
+    printf("%d", i);
     printf("The unsorted list is : \n");
     display(a, i);
     sort(0, i - 1);
-    printf("The sorted list is : \n");
+    printf("\nThe sorted list is : \n");
     display(a, i);
 }
