865ca4951c7dd311136dcc646cb8c2bff63d5b8b
GussySussy <gourisankar.shaji@gmail.com>
2022-12-11 13:49:40 +0530

    fixed quicksort

--- DS LAB/Sem 3/Cycle 3/Quicksort.c
+++ DS LAB/Sem 3/Cycle 3/Quicksort.c
@@ -1,50 +1,64 @@
 #include <stdio.h>
 
-
-void swap(int* x, int* y){
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+void swap(int *i, int *j)
+{
+    int temp = *i;
+    *i = *j;
+    *j = temp;
 }
 
-
-int partition(int arr[], int low, int high){
+int partition(int arr[], int low, int high)
+{
     int pivot = arr[high];
-    int i=low-1;
-    for(int j =0;j<high;j++){
-        if(arr[j]<pivot){
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
             i++;
-            swap(&arr[i],&arr[j]);
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i+1],&arr[high]);
-    return (i+1);
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
 }
 
-
-void quickSort(int arr[],int low,int high){
-    if(low<high){
-        int part=partition(arr,low,high);
-        quickSort(arr,part+1,high);
-        quickSort(arr,low,part-1);
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int part = partition(arr, low, high);
+        quickSort(arr, low, part - 1);
+        quickSort(arr, part + 1, high);
     }
 }
 
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+}
 
-void main(){
-    int size,element;
+void main()
+{
+    int size, element;
     printf("\nEnter the size of the array : ");
-    scanf("%d",&size);
+    scanf("%d", &size);
     int arr[size];
-    for(int i=0;i<size;i++){
+    for (int i = 0; i < size; i++)
+    {
         printf("\nEnter the element : ");
-        scanf("%d",&element);
-        arr[i]=element;
+        scanf("%d", &element);
+        arr[i] = element;
     }
 
-    quickSort(arr,0,size-1);
+    printf("Array before sorting : \n");
+    printArray(arr,size);
 
-    for(int i=0;i<size;i++){
-        printf("%d\t",arr[i]);
-    }
+    quickSort(arr, 0, size - 1);
+
+    printf("\nArray after sorting : \n");
+    printArray(arr,size);
 }
